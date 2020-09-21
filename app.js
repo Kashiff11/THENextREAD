23fd7b9cf26a7c51128a3a0e479748b6058e10f2
Kashiff Khan <abuhafsah@kashiffs-mbp.myfiosgateway.com>
2020-09-21 15:54:39 -0400

    emptied option bar between inputs

--- app.js
+++ app.js
@@ -4,22 +4,43 @@ async function getData(firstName, lastName) {
   try {
     const url = `https://api.nytimes.com/svc/books/v3/reviews.json?author=${firstName}+${lastName}&api-key=eKABUa2Lya4MPuxvFBl9h5919D3o8OYn`
     const response = await axios.get(url);
-    console.log(response.data.results);
+    let list = [];
+    for (let i = 0; i < response.data.results.length; i++) {
+      list.push(response.data.results[i].book_title);
+    }
+    console.log(list)
+    createOption(list);
   } catch (error) {
     console.log(`Error: ${error}`);
   }
 }
 
-function practiceFunction(e) {
+function createVariables(e) {
   e.preventDefault();
   let firstName = document.querySelector('.first').value;
   let lastName = document.querySelector('.last').value
-  console.log(`Author's name: ${firstName} ${lastName}`);
   getData(firstName, lastName)
 }
 
-document.querySelector('button').addEventListener('click', practiceFunction);
+document.querySelector('button').addEventListener('click', createVariables);
 
+function createOption(arr) {
+  emptyOptions();
+  const select = document.querySelector('select');
+  return arr.forEach((title) => {
+    const option = document.createElement('option');
+    option.value = `${title}`;
+    option.textContent = `${title}`;
+    select.append(option);
+  })
+}
+
+function emptyOptions() {
+  const previousOptions = document.querySelector('select');
+  while (previousOptions.hasChildNodes()) {
+    previousOptions.removeChild(previousOptions.firstChild);
+  }
+}
 
 
 
