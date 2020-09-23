a8a6a8f686086189fcaddc2106a1567189b35f67
Kashiff Khan <abuhafsah@kashiffs-mbp.myfiosgateway.com>
2020-09-22 22:14:22 -0400

    Data Comparison

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
+document.querySelector('button').addEventListener('click', createVariables);
+
 async function getData(firstName, lastName) {
+
   try {
     const url = `https://api.nytimes.com/svc/books/v3/reviews.json?author=${firstName}+${lastName}&api-key=eKABUa2Lya4MPuxvFBl9h5919D3o8OYn`
     const response = await axios.get(url);
@@ -7,41 +10,56 @@ async function getData(firstName, lastName) {
     let titleList = [];
 
     if (response.data.results.length == 0) {
+
       noReviews();
-      console.log(`nope`);
+
     } else {
 
-      emptyTitleInfo();   
+      emptyTitleInfo();
 
       for (let i = 0; i < response.data.results.length; i++) {
 
         let titleObject = {
           book: response.data.results[i].book_title,
-          summary: response.data.results[i].summary,                      
-          publicationDate: response.data.results[i].publication_dt,       
+          summary: response.data.results[i].summary,
+          publicationDate: response.data.results[i].publication_dt,
           reviewAuthor: response.data.results[i].byline,
           reviewLink: response.data.results[i].url,
         };
         dataList.push(titleObject);
+
       }
       console.log(dataList);
 
+      //document.querySelector('.buttonTwo').addEventListener('click', optionData);
+
+      //function optionData(e) {
+      // e.preventDefault();
+      // const optionValue = document.querySelector('select').value;
+      // console.log(optionValue);
+      // }
 
       for (let i = 0; i < response.data.results.length; i++) {
-
         let title = response.data.results[i].book_title;
         titleList.push(title);
       }
       console.log(titleList);
     }
     createOption(titleList);
+
+    //document.querySelector('.buttonTwo').addEventListener('click', optionData);
+    //function optionData(e) {
+    //e.preventDefault();
+    //const optionValue = document.querySelector('select').value;
+    //console.log(optionValue);
+    //}
   } catch (error) {
     console.log(`Error: ${error}`);
   }
 }
 
 function emptyOptions() {
-  const previousOptions = document.querySelector('select');         //empties previous options
+  const previousOptions = document.querySelector('select');
   while (previousOptions.hasChildNodes()) {
     previousOptions.removeChild(previousOptions.firstChild);
   }
@@ -51,44 +69,37 @@ function noReviews(arr) {
   emptyOptions();
   emptyTitleInfo();
   const titleInfo = document.querySelector('.title-information');
-  const h2 = document.createElement('h2');
   titleInfo.append(`Sorry, this author has not been reviewd by the NYT`);
 }
 
 function emptyTitleInfo() {
-  const previousTitleInfo = document.querySelector('.title-information');      
+  const previousTitleInfo = document.querySelector('.title-information');
   while (previousTitleInfo.hasChildNodes()) {
     previousTitleInfo.removeChild(previousTitleInfo.firstChild);
   }
 }
-function createVariables(e) {
 
-  e.preventDefault();                                                 
-  let firstName = document.querySelector('.first').value;             
+function createVariables(e) {
+  e.preventDefault();
+  let firstName = document.querySelector('.first').value;
   let lastName = document.querySelector('.last').value;
   getData(firstName, lastName);
-
 }
 
-document.querySelector('button').addEventListener('click', createVariables);
-
 function createOption(arr) {
-
   emptyOptions();
-  const select = document.querySelector('select');                
+  const select = document.querySelector('select');
   return arr.forEach((title) => {
     const option = document.createElement('option');
     option.value = `${title}`;
     option.textContent = `${title}`;
     select.append(option);
   });
-
 }
 
-document.querySelector('select').addEventListener('change', optionData);
-
-function optionData(e) {
-  e.preventDefault();
-  const optionValue = document.querySelector('select').value;
-  console.log(optionValue);
-}
+//document.querySelector('.buttonTwo').addEventListener('click', optionData);
+    //function optionData(e) {
+      //e.preventDefault();
+      //const optionValue = document.querySelector('select').value;
+      //console.log(optionValue);
+    //}
